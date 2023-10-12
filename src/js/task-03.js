63fd4e16ae5209d13ee9a4df209a975ae6446abe
Serhii Osmolovskyi <osmolovsky.s@gmail.com>
2023-10-11 18:42:28 -0600

    HomeWork14

--- src/js/task-03.js
+++ src/js/task-03.js
@@ -11,4 +11,28 @@ const images = [
     url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     alt: 'Group of Horses Running',
   },
+
 ];
+
+const list = document.querySelector('.gallery');
+
+function createMarkup(arr) {
+  const markUp = arr.map(({ url, alt, }) =>
+    `<li>
+    <img src="${url}" alt="${alt}" width = '300'>
+    </li>`);
+  
+  list.insertAdjacentHTML('afterbegin', markUp);
+  console.log(markUp);
+};
+
+list.style.listStyleType = 'none';
+list.style.display = 'flex';
+list.style.flexDirection  = 'row';
+list.style.alignItems = 'center';
+list.style.gap = '24px';
+list.style.justifyContent = 'center';
+list.style.flexWrap = 'wrap';
+
+
+createMarkup(images);
